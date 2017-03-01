47d2749d13d44b0ac60932f3615632611a590892
“Dika <dika.arta@outlook.com>
2017-03-01 15:30:16 +0700

    Using hero class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
+import { Hero } from './Hero'
 
 @Component({
   selector: 'my-app',
   template: `<h1>{{title}}</h1>
-            <h2> My favorite hero is : {{myHero}} </h2>
+            <h2> My favorite hero is : {{myHero.name}} </h2>
             <p>Heroes :</p>
             <ul>
               <li *ngFor="let hero of heroes">
-                  {{hero}}
+                  {{hero.name}}
               </li>
             </ul>
             `
 })
 export class AppComponent  { 
   title = 'Tour of Heroes';
-  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
-  myHero = this.heroes[0];
+
+  heroes : Hero[] = [
+    { id:1, name:'Windstorm' },
+    { id:2, name:'Bombasto' },
+    { id:3, name:'Magneta' },
+    { id:4, name:'Tornado' }, 
+  ]
+
+  myHero: Hero = this.heroes[0];
 }
