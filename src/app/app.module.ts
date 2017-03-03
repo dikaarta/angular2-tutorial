e6d568f55469128d70785c6d872b7cf89d5350d7
“Dika <dika.arta@outlook.com>
2017-03-03 09:04:23 +0700

    Binding to user input events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+//import { AppComponent }  from './app.component';
+import { ClickMeComponent } from './click-me.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ 
+      //AppComponent,
+      ClickMeComponent
+     ],
+  bootstrap:    [ //AppComponent, 
+  ClickMeComponent ]
 })
 export class AppModule { }
