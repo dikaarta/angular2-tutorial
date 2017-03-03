73a5dd6b19092071a8b7c67ed1e37f54269bb6a6
“Dika <dika.arta@outlook.com>
2017-03-03 09:26:25 +0700

    update keyup using template reference variable

--- src/app/keyup.component.ts
+++ src/app/keyup.component.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 @Component({
     selector:"keyup",
     template:`
-        <input (keyup)="onKey($event)">
+        <input #box (keyup)="onKey(box.value)">
         <p>{{values}}</p>
     `
 })
 export class KeyUpComponent_V1{
     values = '';
 
-    onKey(event: KeyboardEvent){
-        this.values += (<HTMLInputElement>event.target).value + ' |';
+    onKey(value: string){
+        this.values += value + ' |';
     }
 }
