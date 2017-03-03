e4e068bc33b7f122b2eb588c5309129c500ff5d4
“Dika <dika.arta@outlook.com>
2017-03-03 09:24:01 +0700

    Get user input from a template reference variable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 //import { AppComponent }  from './app.component';
 import { ClickMeComponent } from './click-me.component';
 import { KeyUpComponent_V1 } from './keyup.component';
+import { LoopBackComponent } from './loop-back.component'
 
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ 
       //AppComponent,
       ClickMeComponent,
-      KeyUpComponent_V1
+      KeyUpComponent_V1,
+      LoopBackComponent
      ],
   bootstrap:    [ //AppComponent, 
   ClickMeComponent,
-  KeyUpComponent_V1]
+  KeyUpComponent_V1,
+  LoopBackComponent]
 })
 export class AppModule { }
