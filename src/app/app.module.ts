a059f413cf3ca3c19eb056a91d218f420050439c
“Dika <dika.arta@outlook.com>
2017-03-03 09:33:01 +0700

    separate key up v1 and v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 //import { AppComponent }  from './app.component';
 import { ClickMeComponent } from './click-me.component';
-import { KeyUpComponent_V1 } from './keyup.component';
+import { KeyUpComponent_V1, KeyUpComponent_V2 } from './keyup.component';
 import { LoopBackComponent } from './loop-back.component'
 
 @NgModule({
@@ -12,11 +12,13 @@ import { LoopBackComponent } from './loop-back.component'
       //AppComponent,
       ClickMeComponent,
       KeyUpComponent_V1,
+      KeyUpComponent_V2,
       LoopBackComponent
      ],
   bootstrap:    [ //AppComponent, 
   ClickMeComponent,
   KeyUpComponent_V1,
+  KeyUpComponent_V2,
   LoopBackComponent]
 })
 export class AppModule { }
--- src/app/keyup.component.ts
+++ src/app/keyup.component.ts
@@ -3,11 +3,26 @@ import { Component } from '@angular/core';
 @Component({
     selector:"keyup",
     template:`
-        <input #box (keyup)="onKey(box.value)">
+        <input (keyup)="onKey($event)">
         <p>{{values}}</p>
     `
 })
 export class KeyUpComponent_V1{
+    values = '';
+
+    onKey(event: any){
+        this.values += event.target.value + ' |';
+    }
+}
+
+@Component({
+    selector:"key-up2",
+    template:`
+        <input #box (keyup)="onKey(box.value)">
+        <p>{{values}}</p>
+    `
+})
+export class KeyUpComponent_V2{
     values = '';
 
     onKey(value: string){
