4f4161b3f9684174bb6ca3e08db1fbe986c56876
“Dika <dika.arta@outlook.com>
2017-03-03 09:41:05 +0700

    on enter

--- src/app/keyup.component.ts
+++ src/app/keyup.component.ts
@@ -29,3 +29,18 @@ export class KeyUpComponent_V2{
         this.values += value + ' |';
     }
 }
+
+@Component({
+    selector: "key-up3",
+    template:`
+        <input #box (keyup.enter)="onEnter(box.value)">
+        <p>{{values}}</p>
+    `
+})
+export class KeyUpComponent_V3{
+    values = '';
+
+    onEnter(value: string){
+        this.values = value;
+    } 
+}
